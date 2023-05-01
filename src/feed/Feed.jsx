d84fa43dec07a9a47f15570689f6a4ccd7c9e6cb
roshan <roshanshibu97@gmail.com>
2023-05-01 20:47:37 +0200

    API integration

--- src/feed/Feed.jsx
+++ src/feed/Feed.jsx
@@ -2,27 +2,44 @@ import Post from "../post/Post"
 import "./Feed.css"
 import FirestoreBanner from '../images/firestore-banner.png';
 import CloudSQLBanner from '../images/cloud-sql-banner.png';
+import Loading from '../images/loading.svg';
+import { useEffect, useState } from "react";
 
 const Feed = (props) => {
-return(
-    <div className="feed-container">
-        <div style={{"text-align": "-webkit-center"}}>
-            <img className="feed-banner" src={props.CloudSQL ? CloudSQLBanner : FirestoreBanner} />
+    let api = "http://127.0.0.1:5000/"
+
+    const [PostJSON, setPostJSON] = useState({});
+    
+    useEffect(() => {
+        setPostJSON({})
+        fetch(api + (props.CloudSQL ?  'sqlPosts' : 'firestorePosts') + '?page=1')
+        .then((response) => response.json())
+        .then((actualData) => setPostJSON(actualData));
+       }, [props.CloudSQL]);
+
+    return(
+        <div className={"feed-container " + (props.CloudSQL ? "blue" : "yellow")}>
+            <div style={{"text-align": "-webkit-center"}}>
+                <img className="feed-banner" src={props.CloudSQL ? CloudSQLBanner : FirestoreBanner} />
+            </div>
+            {
+                Object.keys(PostJSON).length > 0 ?
+                    PostJSON.map((post_data) => {
+                        return (<Post postImage = {post_data.PostImage}
+                            caption={post_data.Caption}
+                            description={post_data.Description}
+                            profile={post_data.CommentImage}
+                            comment={post_data.CommentText}
+                            timestamp={post_data.Timestamp}
+                            key={post_data.ID}
+                            accent={props.CloudSQL ? "blue" : "yellow"}
+                            />)
+                    })
+                :
+                    <img src={Loading} className="loading" />
+            }
         </div>
-        <Post postImage="https://images.unsplash.com/photo-1450778869180-41d0601e046e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0NDA2NTV8MHwxfHNlYXJjaHwxfHxwZXRzfGVufDB8fHx8MTY4MjQzMzAxNQ&ixlib=rb-4.0.3&q=80&w=1080"
-            caption="Caption here"
-            description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of"
-            profile="https://www.w3schools.com/howto/img_avatar.png"
-            comment="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
-        />
-        <Post postImage="https://images.unsplash.com/photo-1450778869180-41d0601e046e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0NDA2NTV8MHwxfHNlYXJjaHwxfHxwZXRzfGVufDB8fHx8MTY4MjQzMzAxNQ&ixlib=rb-4.0.3&q=80&w=1080"
-            caption="Caption here"
-            description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of"
-            profile="https://www.w3schools.com/howto/img_avatar.png"
-            comment="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
-        />
-    </div>
-)
+    )
 }
 
 export default Feed
--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -2,7 +2,7 @@ import "./Post.css"
 
 const Post = (props) => {
 return(
-    <div className="frame">
+    <div className={"frame frame-" + props.accent}>
         <div>
             <img className="image" src={props.postImage} />
         </div>
@@ -10,10 +10,12 @@ return(
         <p className="caption">{props.caption}</p>
         <p className="description smalltext">{props.description}</p>
 
-        <div className="post-comment">
+        <div className={"post-comment comment-" + props.accent}>
             <img className="comment-profile" src={props.profile} />
             <p className="comment smalltext">{props.comment}</p>
         </div>
+
+        <p className="timestamp">{props.timestamp}</p>
     </div>
 )
 }
