939e5d9572728959f6f761d1584f8742b2393716
roshan <roshanshibu97@gmail.com>
2023-05-07 15:50:20 +0200

    Updated post layout - like button

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -1,13 +1,18 @@
 import "./Post.css"
+import Liked from '../images/liked.svg';
+import UnLiked from '../images/unliked.svg';
 
 const Post = (props) => {
 return(
     <div className={"frame frame-" + props.accent}>
-        <div>
-            <img className="image" src={props.postImage} />
+        <img className="image" src={props.postImage} />
+
+        <div className="like-caption-row">
+            <p className="caption">{props.caption}</p>
+            <img className="like-icon" src={props.isLiked ? Liked : UnLiked} />
+            
         </div>
 
-        <p className="caption">{props.caption}</p>
         <p className="description smalltext">{props.description}</p>
 
         <div className={"post-comment comment-" + props.accent}>
