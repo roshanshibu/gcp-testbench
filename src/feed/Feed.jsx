e0f012459aed33b09da1b052f3068f2d78f25508
roshan <roshanshibu97@gmail.com>
2023-05-07 15:50:43 +0200

    contexts for propagating performance stats

--- src/feed/Feed.jsx
+++ src/feed/Feed.jsx
@@ -3,18 +3,49 @@ import "./Feed.css"
 import FirestoreBanner from '../images/firestore-banner.png';
 import CloudSQLBanner from '../images/cloud-sql-banner.png';
 import Loading from '../images/loading.svg';
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { CloudSQLPerfContext, FirestorePerfContext, CloudSQLCountContext, FirestoreCountContext, JSONContext } from '../App';
 
 const Feed = (props) => {
     let api = "http://127.0.0.1:5000/"
 
     const [PostJSON, setPostJSON] = useState({});
     
+    const cloudSQLPerfContext = useContext(CloudSQLPerfContext);
+    const firestorePerfContext = useContext(FirestorePerfContext);
+    const cloudSQLCountContext = useContext(CloudSQLCountContext);
+    const firestoreCountContext = useContext(FirestoreCountContext);
+    const jsonContext = useContext(JSONContext);
+
     useEffect(() => {
-        setPostJSON({})
-        fetch(api + (props.CloudSQL ?  'sqlPosts' : 'firestorePosts') + '?page=1')
+        setPostJSON({});
+        let time1 = performance.now();
+        let random_page = Math.floor(Math.random() * (50 - 1) + 1);
+        fetch(api + (props.CloudSQL ?  'sqlPosts' : 'firestorePosts') + '?page=' + random_page)
         .then((response) => response.json())
-        .then((actualData) => setPostJSON(actualData));
+        .then((actualData) => {setPostJSON(actualData);
+                                jsonContext.setCurrentJSON(actualData);
+
+                                let time2 = performance.now();
+                                console.log(time2 - time1);
+                                if(props.CloudSQL){
+                                    let count = cloudSQLCountContext.CloudSQLCount + 1
+                                    let old_avg = cloudSQLPerfContext.CloudSQLPerf;
+                                    let new_resp_time = time2 - time1;
+                                    let new_avg = old_avg + (new_resp_time - old_avg)/count;
+                                    cloudSQLCountContext.setCloudSQLCount(count);
+                                    cloudSQLPerfContext.setCloudSQLPerf(Number.parseFloat(new_avg));
+                                    //cloudSQLPerfContext.setCloudSQLPerf(new_resp_time);
+                                }else{
+                                    let count = firestoreCountContext.FirestoreCount + 1
+                                    let old_avg = firestorePerfContext.FirestorePerf;
+                                    let new_resp_time = time2 - time1;
+                                    let new_avg = old_avg + (new_resp_time - old_avg)/count;
+                                    firestoreCountContext.setFirestoreCount(count);
+                                    firestorePerfContext.setFirestorePerf(Number.parseFloat(new_avg));
+                                    //firestorePerfContext.setFirestorePerf(new_resp_time);
+                                }
+                                })
        }, [props.CloudSQL]);
 
     return(
@@ -33,6 +64,7 @@ const Feed = (props) => {
                             timestamp={post_data.Timestamp}
                             key={post_data.ID}
                             accent={props.CloudSQL ? "blue" : "yellow"}
+                            isLiked={post_data.isLiked}
                             />)
                     })
                 :
